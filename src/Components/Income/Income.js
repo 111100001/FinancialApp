c85212e8aa8e4e49c4c712405ca5258c2fedaa71
Aamer <aaljagthami1@gmail.com>
2024-11-17 01:08:39 +0300

    commit

---
+++ src/Components/Income/Income.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react'
+import styled from 'styled-components'
+import {useGlobalContext} from '../../context/globalContext'
+import { InnerLayout } from '../../styles/Layouts';
+import Form from '../Form/Form';
+
+function Income() {
+  const {addIncome, incomes, getIncomes} = useGlobalContext()
+
+  useEffect(() =>{
+    getIncomes()
+  }, [])
+  return (
+    <IncomeStyled>
+      <InnerLayout>
+        <h1>Incomes</h1>
+        <div className='income-content'>
+          <div className='form-container'>
+            <Form />
+          </div>
+          <div className='incomes'>
+            {incomes.map((income) => {
+              
+            })}
+          </div>
+        </div>
+      </InnerLayout>
+    </IncomeStyled>
+  )
+}
+
+const IncomeStyled = styled.div`
+
+`;
+
+export default Income
---
+++ src/History/History.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function History() {
+    return (
+        <HistoryStyled>
+            <h2>Recent History</h2>
+            <div className='history-item'>
+                <p style={{color:'red'}}>
+                    Recent Expense
+                </p>                
+            </div>            
+            <div className='history-item'>
+                <p style={{color: 'var(--color-green)'}}>
+                    Recent Income
+                </p>                
+            </div>
+        </HistoryStyled>
+    )
+}
+
+const HistoryStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    .history-item{
+        background: #626670;
+        border: 2px solid #000000;
+        box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+        padding: 1rem;
+        border-radius: 20px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+`;
+
+export default History
